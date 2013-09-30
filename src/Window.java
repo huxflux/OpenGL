ee59971def484d020b2b578e906ef4e25e4d877a
huxflux <johnis@student.hist.no>
2013-09-30 21:41:20 +0200

    Div scheiss

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,5 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -18,7 +17,7 @@ public class Window {
         float           x = 400;
         float           y = 300;
         float           rotation = 0;
-        float           angleStep = 0.25f;
+        float           angleStep = 3.0f;
 
         try {
             Display.setDisplayMode(new DisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -27,24 +26,29 @@ public class Window {
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
+
+        System.out.println(GL11.glGetString(GL11.GL_VERSION));
+        System.out.println(GL11.glGetString(GL11.GL_VENDOR));
+        System.out.println(GL11.glGetString(GL11.GL_POINT_SIZE));
+
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         GL11.glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        GL11.glPolygonMode( GL11.GL_FRONT_AND_BACK, GL11.GL_LINE );
+        GL11.glPolygonMode( GL11.GL_FRONT_AND_BACK, GL11.GL_LINES);
 
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
         GL11.glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 1, -1);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glLineWidth(4);
+        GL11.glLineWidth(3);
 
         while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
             GL11.glColor3f(0.0f, 0.6f, 0.3f);
 
-            if (Mouse.isButtonDown(0)) {
+            if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
                 rotation += angleStep;
-            } else if (Mouse.isButtonDown(1)) {
+            } else if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
                 rotation -= angleStep;
             }
 
@@ -53,6 +57,10 @@ public class Window {
             GL11.glRotatef(rotation, 0.0f, 0.0f, 1.0f);
             GL11.glTranslatef(-x, -y, 0);
 
+//            GL11.glTranslatef(triangle_X1, triangle_Y1, 0.0f);
+//            GL11.glRotatef(rotation, 0.0f, 0.0f, 1.0f);
+//            GL11.glTranslatef(-triangle_X1, -triangle_Y1, 0.0f);
+
             GL11.glBegin(GL11.GL_TRIANGLES);
             GL11.glVertex2f(triangle_X1, triangle_Y1);
             GL11.glVertex2f(triangle_X2, triangle_Y2);
@@ -60,6 +68,7 @@ public class Window {
             GL11.glEnd();
             GL11.glPopMatrix();
 
+            Display.sync(60);
             Display.update();
         }
         Display.destroy();
